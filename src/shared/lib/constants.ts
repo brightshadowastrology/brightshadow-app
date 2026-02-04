e97c1b8361024982af41bd179249bc5961ff4082
Singithi Kandage <singithi.kandage@gmail.com>
2026-02-04 14:30:35 -0500

    feat: new libraries

---
+++ src/shared/lib/constants.ts
@@ -0,0 +1,57 @@
+export const bodySymbol = ["☉", "☽", "☿", "♀", "♂", "♃", "♄", "♅", "♆", "♇"];
+export const signSymbol = [
+  "♈︎",
+  "♉︎",
+  "♊︎",
+  "♋︎",
+  "♌︎",
+  "♍︎",
+  "♎︎",
+  "♏︎",
+  "♐︎",
+  "♑︎",
+  "♒︎",
+  "♓︎",
+];
+
+export const months = [
+  { label: "January", value: "01" },
+  { label: "February", value: "02" },
+  { label: "March", value: "03" },
+  { label: "April", value: "04" },
+  { label: "May", value: "05" },
+  { label: "June", value: "06" },
+  { label: "July", value: "07" },
+  { label: "August", value: "08" },
+  { label: "September", value: "09" },
+  { label: "October", value: "10" },
+  { label: "November", value: "11" },
+  { label: "December", value: "12" },
+];
+
+export const houseTopics: { [key: number]: string[] } = {
+  1: ["self", "body", "identity"],
+  2: ["money", "assets", "self esteem"],
+  3: ["learning", "local travel", "communication", "siblings"],
+  4: ["home", "family", "ancestry", "property"],
+  5: ["creativity", "children", "fun", "romance", "pleasure"],
+  6: ["daily habits", "service", "health", "pets"],
+  7: ["one-to-one relationships", "contracts", "open enemies"],
+  8: ["debt", "taxes", "shared resources", "subconscious material"],
+  9: [
+    "worldview",
+    "higher education",
+    "religion",
+    "long distance travel",
+    "legal matters",
+  ],
+  10: ["career", "vocation", "public reputation", "authority figures"],
+  11: [
+    "friendships",
+    "professional networks",
+    "supporters",
+    "community",
+    "aspirations",
+  ],
+  12: ["solitude", "rest", "self-undoing", "hidden enemies", "spirituality"],
+};
